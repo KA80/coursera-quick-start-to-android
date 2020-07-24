db86267821553defd4ae5bb7eb866099be3f8d2c
Stanislav Nagornov <58802975+KA80@users.noreply.github.com>
2020-07-24 19:15:29 +1000

    Создание ProfileActivity, переход из AuthActivity в ProfileActivity, передача параметров

--- app/src/main/java/com/example/appproject/AuthActivity.java
+++ app/src/main/java/com/example/appproject/AuthActivity.java
@@ -3,6 +3,7 @@ package com.example.appproject;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
@@ -22,7 +23,11 @@ public class AuthActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             if (isEmailValid() && isPasswordValid()) {
-                //TODO переход в приложение
+                Intent startProfileIntent = new Intent(AuthActivity.this,
+                        ProfileActivity.class);
+                startProfileIntent.putExtra(ProfileActivity.EMAIL_KEY, mLogin.getText().toString());
+                startProfileIntent.putExtra(ProfileActivity.PASSWORD_KEY, mPassword.getText().toString());
+                startActivity(startProfileIntent);
             } else {
                 showMessage(R.string.login_input_error);
             }
---
+++ app/src/main/java/com/example/appproject/ProfileActivity.java
@@ -0,0 +1,43 @@
+package com.example.appproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    public static String EMAIL_KEY = "EMAIL_KEY";
+    public static String PASSWORD_KEY = "PASSWORD_KEY";
+
+
+    private AppCompatImageView mPhoto;
+    private TextView mLogin;
+    private TextView mPassword;
+
+    private View.OnClickListener mOnPhotoClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //TODO
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_profile);
+
+        mPhoto = findViewById(R.id.ivPhoto);
+        mLogin = findViewById(R.id.tvEmail);
+        mPassword = findViewById(R.id.tvPassword);
+
+        Bundle bundle = getIntent().getExtras();
+        mLogin.setText(bundle.getString(EMAIL_KEY));
+        mPassword.setText(bundle.getString(PASSWORD_KEY));
+
+        mPhoto.setOnClickListener(mOnPhotoClickListener);
+    }
+}
